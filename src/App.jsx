cdef1df5cfe16de92e05933a5ea1300be3a7a589
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-26 00:55:37 +0530

    worling on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 
-import { useState } from 'react'
+
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
-  const[count,setCount]=useState(0);
+ 
 
   return (
-    <div className='bg-gray-300'>
-      
-        {count}
+    <div className="bg-[#050414]">
+       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
+       </div>
+       <div className="relative pt-20">
+         <Navbar/>
+       </div>
+       hello
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
+  const menuItems = [
+    {id:"About",label:"About"},
+    {id:"skills",label:"Skills"},
+    {id:"Experience",label:"Experience"},
+    {id:"project",label:"Projects"},
+    {id:"contact",label:"Contact"}
+
+  ]
+    const[activeSection, setActiveSection] = useState("");
+    const [isScrolled, setIsScrolled] = useState(false);   
+
+  
   return (
-    <div>
+    <nav  className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] ${
+      isScrolled ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md" : "bg-transparent"
+    }`}>
+      <div className="text-white py-5 flex justify-between items-center">
+          <div className="text-lg font-semibold cursor-pointer">
+               <span  className="text-[#8245ec]">
+                  &lt;
+               </span>
+               <span className='text-white'>
+                 Vipul
+               </span>
+               <span className='text-white'>
+                 /
+               </span>
+               <span className='text-white'>
+                  Kumar
+               </span>
+               <span className="text-[#8245ec]">
+                  &gt;
+               </span>
+          </div>
+          <ul className="hidden md:flex space-x-8 text-gray-300">
+              {
+                 menuItems.map((item)=>{
+                     return (
+                      <li
+                      key={item.id}
+                      onClick={() => setActiveSection(item.id)}
+                      className={`cursor-pointer  hover:text-[#8245ec] ${
+                        activeSection === item.id ? 'text-[#8245ec]' : ''
+                      }`}
+                    >
+                              <button className={`hover:text-[#8245ec] ${
+    activeSection === item.id ? 'text-[#8245ec]' : 'text-gray-300'
+  } cursor-pointer`} >
+                                 {item.label}
+                              </button>
+                            </li>
+                     )
+                            
+                     
+                 })
+              }
+          </ul>
+           
+      </div>
       
-    </div>
+    </nav>
   )
 }
 
